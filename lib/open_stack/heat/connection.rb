68ffe685f21b7bc6133057e1062b0c77be547d4f
Rob Sweet <rsweet@hooker.local>
2015-09-27 21:35:00 -0400

    WIP

--- lib/open_stack/heat/connection.rb
+++ lib/open_stack/heat/connection.rb
@@ -1,64 +1,41 @@
 module OpenStack
-module HeatCfn
+  module HeatCfn
+    class Connection
 
-  class Connection
+      attr_accessor   :connection
+      attr_reader     :stacks
 
-    attr_accessor   :connection
-    attr_reader     :stacks
+      def initialize(connection)
+        @connection = connection
+        OpenStack::Authentication.init(@connection)
+        @stack_path = "#{@connection.authtenant_name}/stacks"
+      end
 
-    def initialize(connection)
-      @connection = connection
-      OpenStack::Authentication.init(@connection)
-      @stack_path = "#{@connection.authtenant_name}/stacks"
+      # Returns true if the authentication was successful and returns false otherwise.
+      #
+      #   cs.authok?
+      #   => true
+      def authok?
+        @connection.authok
+      end
+
+
+
+
+
+
+
+
+      def delete_stack(stack_id)
+        get_stack(stack_id).delete
+      end
+
+      private
+
+
+
+
     end
-
-    # Returns true if the authentication was successful and returns false otherwise.
-    #
-    #   cs.authok?
-    #   => true
-    def authok?
-      @connection.authok
-    end
-
-    #require params:  {:display_name, :size}
-    #optional params: {:display_description, :metadata=>{:key=>val, ...}, :availability_zone, :stack_type }
-    #returns OpenStack::Volume::Volume object
-    def create_stack(options)
-      # raise OpenStack::Exception::MissingArgument, ":display_name and :size must be specified to create a stack" unless (options[:display_name] && options[:size])
-      data = JSON.generate(:stack => options)
-      response = @connection.csreq("POST",@connection.service_host,"#{@connection.service_path}/#{@stack_path}",@connection.service_port,@connection.service_scheme,{'content-type' => 'application/json'},data)
-      OpenStack::Exception.raise_exception(response) unless response.code.match(/^20.$/)
-      stack_info = JSON.parse(response.body)["stack"]
-      stack = OpenStack::Volume::Volume.new(stack_info)
-    end
-
-    #no options documented in API at Nov 2012
-    #(e.g. like limit/marker as used in Nova for servers)
-    def list_stacks
-      response = @connection.req("GET", "/#{@stack_path}")
-      stacks_hash = JSON.parse(response.body)["stacks"]
-      stacks_hash.inject([]){|res, current| res << OpenStack::Heat::Stack.new(current); res}
-    end
-    alias :stacks :list_stacks
-
-
-    def get_stack(stack_id)
-      response = @connection.req("GET", "/#{@stack_path}/#{stack_id}")
-      stack_hash = JSON.parse(response.body)["stack"]
-      OpenStack::Volume::Volume.new(stack_hash, @connection)
-    end
-    alias :stack :get_stack
-
-    def delete_stack(stack_id)
-      get_stack(stack_id).delete
-    end
-
-    private
-
-
-
 
   end
-
-end
 end
--- lib/open_stack/heat/stack.rb
+++ lib/open_stack/heat/stack.rb
@@ -14,6 +14,41 @@ module OpenStack
     attr_reader :created_at
     attr_reader :status
 
+    #require params:  {:display_name, :size}
+    #optional params: {:display_description, :metadata=>{:key=>val, ...}, :availability_zone, :stack_type }
+    #returns OpenStack::Volume::Volume object
+    def self.create options
+      # raise OpenStack::Exception::MissingArgument, ":display_name and :size must be specified to create a stack" unless (options[:display_name] && options[:size])
+      data = JSON.generate :stack => options
+      response = @connection.csreq "POST",
+                                   @connection.service_host,
+                                   "#{@connection.service_path}/#{@stack_path}",
+                                   @connection.service_port,
+                                   @connection.service_scheme,
+                                   {'content-type' => 'application/json'},
+                                   data
+
+      OpenStack::Exception.raise_exception(response) unless response.code.match(/^20.$/)
+      stack_info = JSON.parse(response.body)["stack"]
+      stack = OpenStack::Heat::Stack.new(stack_info)
+    end
+
+    #no options documented in API at Nov 2012
+    #(e.g. like limit/marker as used in Nova for servers)
+    def self.stacks
+      response = @connection.req("GET", "/#{@stack_path}")
+      stacks_hash = JSON.parse(response.body)["stacks"]
+      stacks_hash.inject([]){|res, current| res << OpenStack::Heat::Stack.new(current); res}
+    end
+    alias :stacks :list_stacks
+
+    def self.find(stack_id)
+      response = @connection.req("GET", "/#{@stack_path}/#{stack_id}")
+      stack_hash = JSON.parse(response.body)["stack"]
+      OpenStack::Volume::Volume.new(stack_hash, @connection)
+    end
+    alias :stack :find
+
     def initialize(stack_info, connection)
       @conn                = connection
       @id                  = stack_info['id']
